0ff27a77f38b49dc37105ccc54601d881add63dd
Tadas V <tadovas@gmail.com>
2020-01-24 20:32:32 +0000

    Add basic http server with graceful stop

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	server := http.Server{}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, os.Kill)
+	go func() {
+		<-stop
+		if err := server.Close(); err != nil {
+			fmt.Println("Service stop error:", err)
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		fmt.Println("Http service error:", err)
+	}
+	fmt.Println("Terminated")
 }
